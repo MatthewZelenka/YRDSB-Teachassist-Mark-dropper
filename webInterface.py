529fb0436e205d854a9be761a704cc7226679c42
Matthew Zelenka <MZelenka04@gmail.com>
2022-06-23 23:58:31 -0400

    Fixed if statment

--- webInterface.py
+++ webInterface.py
@@ -40,7 +40,7 @@ def getCourses(session:requests.Session, userPageUrl:str):
                 "timeSlot":list(row)[0].getText().split("  ")[1].split("-")[0].lstrip().rstrip(),
                 "roomNumber":list(row)[0].getText().split("  ")[1].split("-")[1].lstrip().rstrip(),
                 "date":list(row)[1].getText(),
-                "mark": float(list(row)[3].getText().split("current mark =")[-1].lstrip().rstrip().removesuffix('%')) if list(row)[3].getText().split("current mark =")[-1].lstrip().rstrip().removesuffix('%') else None,
+                "mark": float(list(row)[3].getText().split("current mark =")[-1].lstrip().rstrip().removesuffix('%')) if list(row)[3].getText().split("current mark =")[-1].lstrip().rstrip().removesuffix('%') and "Please see teacher for current status regarding achievement in the course" not in list(row)[3].getText().split("current mark =")[-1].lstrip().rstrip().removesuffix('%') else None,
                 "url":url+"students/"+_getUrl(list(row)[3]) if "Please see teacher for current status regarding achievement in the course" not in list(row)[3].getText().split("current mark =")[-1].lstrip().rstrip().removesuffix('%') else None,
             } for row in tables[_courseTableIndex(tables=tables, header=['Course Name', 'Date', 'Mark'])].findAll('tr')[1:]
         ] #
@@ -56,8 +56,25 @@ def getCourseData(session:requests.Session, coursePageUrl:str):
     # for row in tables[_courseTableIndex(tables=tables, header=['Assignment', 'Knowledge / Understanding', 'Thinking', 'Communication', 'Application'])].findAll('tr')[1:]: # code to see table
     #     print([col.getText() for col in row])
     course = {
-        "weighting":{list(row)[0].getText().replace(" ", ""): list(row)[-2].getText().removesuffix('%') for row in tables[_courseTableIndex(tables=tables, header=['Category', 'Weighting', 'Course Weighting', 'Student Achievement'])].findAll('tr')[1:]},
-        "assignment":{list(row)[0].getText():{'Knowledge/Understanding':{"mark":eval(list(row)[1].getText().split("=")[0]) if list(row)[1].getText() != "" and "no mark" not in list(row)[1].getText() and "no weight" not in list(row)[1].getText() else None,"weight":list(row)[1].getText().split("=")[-1] if list(row)[1].getText() != "" and "no mark" not in list(row)[1].getText() and "no weight" not in list(row)[1].getText() else None}, 'Thinking':{"mark":eval(list(row)[2].getText().split("=")[0]) if list(row)[2].getText() != "" and "no mark" not in list(row)[2].getText() and "no weight" not in list(row)[2].getText() else None,"weight":list(row)[2].getText().split("=")[-1] if list(row)[2].getText() != "" and "no mark" not in list(row)[2].getText() and "no weight" not in list(row)[2].getText() else None}, 'Communication':{"mark":eval(list(row)[3].getText().split("=")[0]) if list(row)[3].getText() != "" and "no mark" not in list(row)[3].getText() and "no weight" not in list(row)[3].getText() else None,"weight":list(row)[3].getText().split("=")[-1] if list(row)[3].getText() != "" and "no mark" not in list(row)[3].getText() and "no weight" not in list(row)[3].getText() else None}, 'Application':{"mark":eval(list(row)[4].getText().split("=")[0]) if list(row)[4].getText() != "" and "no mark" not in list(row)[4].getText() and "no weight" not in list(row)[4].getText() else None,"weight":list(row)[4].getText().split("=")[-1] if list(row)[4].getText() != "" and "no mark" not in list(row)[4].getText() and "no weight" not in list(row)[4].getText() else None}} for row in tables[_courseTableIndex(tables=tables, header=['Assignment', 'Knowledge / Understanding', 'Thinking', 'Communication', 'Application'])].findAll('tr')[1:] if len(row) > 1
+        "weighting":{
+            list(row)[0].getText().replace(" ", ""): list(row)[-2].getText().removesuffix('%') for row in tables[_courseTableIndex(tables=tables, header=['Category', 'Weighting', 'Course Weighting', 'Student Achievement'])].findAll('tr')[1:]
+        },
+        "assignment":{
+            list(row)[0].getText():{
+                'Knowledge/Understanding':{
+                    "mark":(eval(list(row)[1].getText().split("=")[0]) if list(row)[1].getText() != "" else None) if "no mark" not in list(row)[1].getText() else None,
+                    "weight":((list(row)[1].getText().split("=")[-1] if list(row)[1].getText() != "" else None) if "no weight" not in list(row)[1].getText() else "0") if "no mark" not in list(row)[1].getText() else None
+                }, 'Thinking':{
+                    "mark":(eval(list(row)[2].getText().split("=")[0]) if list(row)[2].getText() != "" else None) if "no mark" not in list(row)[2].getText() else None,
+                    "weight":((list(row)[2].getText().split("=")[-1] if list(row)[2].getText() != "" else None) if "no weight" not in list(row)[2].getText() else "0") if "no mark" not in list(row)[2].getText() else None
+                }, 'Communication':{
+                    "mark":(eval(list(row)[3].getText().split("=")[0]) if list(row)[3].getText() != "" else None) if "no mark" not in list(row)[3].getText() else None,
+                    "weight":((list(row)[3].getText().split("=")[-1] if list(row)[3].getText() != "" else None) if "no weight" not in list(row)[3].getText() else "0") if "no mark" not in list(row)[3].getText() else None
+                }, 'Application':{
+                    "mark":(eval(list(row)[4].getText().split("=")[0]) if list(row)[4].getText() != "" else None) if "no mark" not in list(row)[4].getText() else None,
+                    "weight":((list(row)[4].getText().split("=")[-1] if list(row)[4].getText() != "" else None) if "no weight" not in list(row)[4].getText() else "0") if "no mark" not in list(row)[4].getText() else None
+                }
+            } for row in tables[_courseTableIndex(tables=tables, header=['Assignment', 'Knowledge / Understanding', 'Thinking', 'Communication', 'Application'])].findAll('tr')[1:] if len(row) > 1
         }
     }
     # print(course)
@@ -69,4 +86,4 @@ if __name__ == '__main__':
         print(responseData.url)
         coursesData = getCourses(session=s, userPageUrl=responseData.url)
         print(coursesData)
-        getCourseData(session=s, coursePageUrl=coursesData[1]["url"])
+        print(getCourseData(session=s, coursePageUrl=coursesData[0]["url"]))
